db7a8a8918f4d57b21f89b171feeb1d6bbcf810e
Ian Melhorn <ijmelhorn@gmail.com>
2022-02-05 12:32:08 -0500

    erc-20s deployed

--- ERC-20/KYTE.sol
+++ ERC-20/KYTE.sol
@@ -1,15 +1,12 @@
 pragma solidity ^0.5.0;
 
 //  Import the following contracts from the OpenZeppelin library:
-//    * `ERC20`
-//    * `ERC20Detailed`
-//    * `ERC20Mintable`
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 // Create a constructor for the KaseiCoin contract
-contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
+contract KYTE is ERC20, ERC20Detailed, ERC20Mintable {
     constructor(
         string memory name,
         string memory symbol,
